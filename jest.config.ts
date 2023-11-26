6f904391c8b8089d791bf790dc0c3c1bd8dd17bd
Jake Shilling <shilling.jake@gmail.com>
2023-11-26 07:06:20 -0500

    FIX tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,13 @@
+import { pathsToModuleNameMapper } from 'ts-jest';
 import type { JestConfigWithTsJest } from 'ts-jest';
+
+import { compilerOptions as baseCompilerOptions } from './tsconfig.base.json';
+import { compilerOptions as testCompilerOptions } from './tsconfig.test.json';
+
+const compilerOptions = {
+  ...baseCompilerOptions,
+  ...testCompilerOptions,
+};
 
 const jestConfig: JestConfigWithTsJest = {
   preset: 'ts-jest',
@@ -12,6 +21,11 @@ const jestConfig: JestConfigWithTsJest = {
   },
   testEnvironment: 'node',
   testMatch: ['<rootDir>/test/**/*.ts'],
+  roots: ['<rootDir>'],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  })!,
 };
 
 export default jestConfig;
--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,5 @@
 import { expect, test } from '@jest/globals';
-import { double } from 'src/index.js';
+import { double } from '@functorfactory/create-lib';
 
 test('double', () => {
   expect(double(1)).toBe(2);
